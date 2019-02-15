f9f77d4cac7abf380ae2252144c3dd308d6019ae
Han Quoc Hien <44970568+khunglongFBI@users.noreply.github.com>
2019-02-15 09:29:23 +0700

    first commit

---
+++ vsii/src/main/java/com/vn/vsii/Service/AccountService.java
@@ -0,0 +1,20 @@
+package com.vn.vsii.Service;
+
+import com.vn.vsii.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+public interface AccountService {
+Page<Account> findAll(Pageable pageable);
+    Account findById(Long id);
+    void save(Account user);
+
+    void remove(Long id);
+
+    void setupRole(String role,Account account);
+
+    Page<Account> findAllByStudentName(String nickname, Pageable pageable);
+
+    Account findByUserName(String username);
+}
---
+++ vsii/src/main/java/com/vn/vsii/reponsitory/AccountRepository.java
@@ -0,0 +1,16 @@
+package com.vn.vsii.reponsitory;
+
+import com.vn.vsii.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface AccountRepository extends PagingAndSortingRepository<Account,Long> {
+    Account findByUserName(String username);
+    Page<Account> findAll(Pageable pageable);
+    Optional<Account> findAllById(Long id);
+}
